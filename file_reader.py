79384e9262ac183e23e339959191dc50afb6b6a5
Deserae <deserae@ucsb.edu>
2026-05-03 19:35:09 -0700

    new updates for file_reader

--- file_reader.py
+++ file_reader.py
@@ -14,14 +14,32 @@ class FileReader:
         """
         Returns a binary string of the file contents, or None.
         """
-        if filepath == file{
-            return 
-        }
-        filepath = open("filepath.txt", "r")
-        return None
+        #checks if its a file
+        if os.path.exists(filepath):
+            if os.path.isfile(filepath):
+                read = open(filepath,"rb")
+                read.close()
+                return read.read()
+            if os.path.isdir(filepath):
+                result = ""
+                directoryF = os.listdir(filepath)
+                for d in directoryF:
+                    result += f"<li> {d} </li>"
+                return f"<html><body><ul> {result} </ul></body></html>"
+        else: 
+            return None
 
     def head(self, filepath, cookies):
         """
         Returns the size to be returned, or None.
         """
-        return None
+        if os.path.exists(filepath):
+            if os.path.isfile(filepath): 
+                read = open(filepath,"rb")
+                size = len(read.read())
+                read.close()
+                return size
+            if os.path.isdir(filepath):
+                return len(self.get(filepath))
+        else:
+            return None
